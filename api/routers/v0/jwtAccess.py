503bd4b29f43943cafa785d5291d7dfb9cc57458
andy9310 <r12921a25@ntu.edu.tw>
2024-07-11 16:45:37 +0800

    finish jwt token verification

--- api/routers/v0/jwtAccess.py
+++ api/routers/v0/jwtAccess.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 from fastapi import APIRouter, Body, HTTPException
 from datetime import datetime
 from ...logger import logger
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from datetime import datetime, timezone, timedelta
 from cryptography.fernet import Fernet
 import hashlib
@@ -21,6 +21,8 @@ class JWTResponse(BaseModel):
     message: bool = Field(examples=[True]),
     jwt_token: str = Field(examples=["jwt_token"])
 
+class JWTrequest(BaseModel):
+    token: str = Field(examples=["jwt_token"])
 # async def forward(message):
 #     post_url = f"{_CORE_SERVICE_URL}/api/v0/question"
 #     try:
@@ -105,7 +107,7 @@ def load_secret_file(file_path):
             if line.strip() and not line.startswith('#'):
                 key, value = line.strip().split('=', 1)
                 secret_vars[key] = value
-    return secret_vars
+    return secret_vars['hashed_jwt']
 
 def write_secret_file(env_vars):
     generate_and_save_key()
@@ -113,16 +115,27 @@ def write_secret_file(env_vars):
         for key, value in env_vars.items():
             file.write(f"{key}={value}\n")
     encrypt_file("secret.txt")
-
+def string_to_hex(s):
+    hex_string = ''.join(format(ord(c), '02x') for c in s)
+    return hex_string
 def verify( jwt_token:str ):
-    decrypted_file_name = decrypt_file("encrypted_secrets.txt")
+    decrypted_file_name = decrypt_file("encrypted_secret.txt")
     secrets = load_secret_file(decrypted_file_name)
-    hashed_jwt_bytes = bytes.fromhex( secrets )
-    salt = hashed_jwt_bytes[:16]
-    stored_hash = hashed_jwt_bytes[16:]
-    hash_object = hashlib.sha256(salt + jwt_token.encode())
+    print(secrets)
+    salt = secrets[:16].encode()  # Adjust based on how the salt and stored hash are stored
+    stored_hash = secrets[:].encode()  # Adjust based on how the salt and stored hash are stored
+
+    # Create a new hash with the salt and jwt_token
+    hash_object = hashlib.sha256( jwt_token.encode())
     new_hash = hash_object.digest()
-    if new_hash == stored_hash:
+    # hashed_jwt_bytes = bytes.fromhex( secrets )
+    # salt = hashed_jwt_bytes[:16]
+    # stored_hash = hashed_jwt_bytes[16:]
+    # hash_object = hashlib.sha256(salt + jwt_token.encode())
+    # new_hash = hash_object.digest()
+    print(f"new: {jwt_token.encode()}")
+    print(f"stored: {stored_hash}")
+    if jwt_token.encode() == stored_hash:
         return True
     else:
         return False
@@ -150,25 +163,27 @@ async def jwtGenerator():
     """The API to generate jwt token initially."""
     # token = os.getenv("SECRET_KEY")
     token = "seed_key_for_chat_agent_of_intelligentQA"
-    print(f"token: {token}")
+    # print(f"token: {token}")
     new_token = generate_jwt(token)
+    print(f"token: {new_token}")
     write_secret_file({"hashed_jwt":new_token})
     return {"status": "success"}
 
 @router.post("/", status_code=200)
-async def jwtGenerator(request: Annotated[str, Body(media_type="application/json")]):
+async def jwtGenerator(request: Annotated[JWTrequest, Body(media_type="application/json")]):
     """The API to Ask questions."""
     try:
         send_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+        
         logger.info("[INFO][Chat-Agent][PostJWT]receive request: %s, %s",
-                    json.dumps(request), send_timestamp)
+                    json.dumps(request.dict()), send_timestamp)
         ## jwt verify and create next new jwt for client side
         ##
-        print(request['token'])
+        #print(request.dict()['token'])
         ## 
         response = {}
-        if verify(request['token']):
-            token = os.getenv("SECRET_KEY")
+        if verify(request.dict()['token']):
+            token = "seed_key_for_chat_agent_of_intelligentQA"
             new_token = generate_jwt(token)
             write_secret_file({"hashed_jwt":new_token})
             response['message'] = True
